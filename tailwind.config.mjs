9b8a94da4da363331b6e1867fccfa14ece809efc
Yustin Musidi <yustinmusidi@gmail.com>
2024-09-08 11:53:37 +0700

    update halaman

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,24 @@ export default {
 	theme: {
 		extend: {},
 	},
+	daisyui: {
+		themes: [
+			{
+				sadayana: {
+					"primary": "#495d49",
+					"secondary": "#4a5363",
+					"accent": "#b4c5de",
+					"neutral": "#f7f4ef",
+					"base-100": "#ffffff",
+					"info": "#3b82f6",	
+					"success": "#a3e635",		
+					"warning": "#fbbf24",		
+					"error": "#dc2626",
+				},
+			},
+		],
+	},
 	plugins: [
-		require('flowbite/plugin')
+		require('daisyui')
 	],
 }
