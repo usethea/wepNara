9c0fa428b7af08a401e57692063b8a25cbf0e095
Yustin Musidi <yustinmusidi@gmail.com>
2024-09-16 13:53:53 +0700

    add api key

--- src/pages/actions/send-email.json.ts
+++ src/pages/actions/send-email.json.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 import { Resend } from "resend";
 
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const resend = new Resend(import.meta.env.RESEND_KEY);
 
 export const GET:APIRoute = async ({params, request}) => {
     return new Response(
